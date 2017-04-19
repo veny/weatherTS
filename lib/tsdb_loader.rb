4536a1f708fced0f739e3b1e1d0d18ac3db8f40e
veny <vaclav.sykora@gmail.com>
2017-04-19 22:39:23 +0200

    cosmetic

--- lib/tsdb_loader.rb
+++ lib/tsdb_loader.rb
@@ -5,22 +5,22 @@ module WeatherTS
 
     def exec
       data = context[:transformed]
+      log.info "loading data matrix #{data[:pixels].size}x#{data[:pixels][0].size}, timestamp=#{data[:timestamp]}"
       data[:pixels].each_with_index do |b, x|
         b.each_with_index do |val, y|
           point = { tags: {}, values: {}, timestamp: data[:timestamp] }
           point[:tags][:x] = x
           point[:tags][:y] = y
-        #   point[:values][:x] = x
-        #   point[:values][:y] = y
-          point[:values][:value] = "#{val}i" # write as integer
-        #   if x % 10 == 0
-            print "\r"
-            # print "progress: #{x / 10} %"
-            print "progress: #{x} % | #{point}"
-            $stdout.flush
+          point[:values][:value] = val
+
+          # progress
+          print "\r"
+          print "progress: #{x * 100 / data[:pixels].size} %"
+          $stdout.flush
           App.instance.service(:dao).insert('z_max3d', point) #if x == 0 and y == 0
         end
       end
+      print "\r"
     end
 
   end
